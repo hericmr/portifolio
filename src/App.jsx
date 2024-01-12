9bd845cb3e8e743544b60b7b32c6a799deeafb42
Héric Moura <111160949+hericmr@users.noreply.github.com>
2024-01-12 16:34:11 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Héric Moura",
+  title: "Fotógrafo,    web-designer e Desenvolvedor Python",
+  email: "heric.moura@unifesp.com",
+  gitHub: "hericmh",
+  instagram: "heric.mr",
+  linkedIn: "",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
